ce58556e17baf2d7977422b8bc105373cda624c0
SkaterDanny <dannykruiswijk@hotmail.com>
2015-12-14 11:41:14 +0100

    Added Leap Movement, Mouse Input, Boundaries

---
+++ Leap Motion Test/Assets/Scripts/PlayerLeap.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLeap : MonoBehaviour {
+
+    private GameObject leapObject;
+    private float rotationValueLeft = 0;
+    private float rotationValueRight = 0;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        //Transform Rotation Back = Rechts
+        //Transform Rotation Forward = Links
+        transform.Rotate(Vector3.back * rotationValueRight);
+        transform.Rotate(Vector3.forward * rotationValueLeft);
+        leapObject = GameObject.Find("InvisHandObject(Clone)");
+        /*
+        if (leapObject.transform.position.x < transform.position.x)
+        {
+            rotationValueLeft = 1;
+        }
+        else
+        {
+            rotationValueLeft = 0;
+        }
+        if (leapObject.transform.position.x > transform.position.x)
+        {
+            rotationValueRight = 0;
+        }
+        else
+        {
+            rotationValueRight = 0;
+        }
+        */
+        if (leapObject != null)
+        {
+            transform.position = leapObject.transform.position;
+        }
+        //Player Boundaries
+        if (transform.position.y < 0.1f)
+        {
+            transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z);
+        }
+        if (transform.position.y > 2f)
+        {
+            transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+        }
+        if (transform.position.x < -1.7f)
+        {
+            transform.position = new Vector3(-1.7f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 1.7f)
+        {
+            transform.position = new Vector3(1.7f, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Leap Motion Test/Assets/Scripts/PlayerMouse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMouse : MonoBehaviour {
+
+    Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = 2;
+	    mousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        transform.position = mousePosition;
+
+        //Player Boundaries
+        if (transform.position.y < 0.1f)
+        {
+            transform.position = new Vector3(transform.position.x,0.1f,transform.position.z);
+        }
+        if (transform.position.y > 2f)
+        {
+            transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+        }
+        if (transform.position.x < -1.7f)
+        {
+            transform.position = new Vector3(-1.7f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 1.7f)
+        {
+            transform.position = new Vector3(1.7f, transform.position.y, transform.position.z);
+        }
+    }
+}
