7f3756bc3c5970708ec516209b26a3301d4cd24c
Niels <Niels97vet@hotmail.com>
2016-01-15 21:32:46 +0100

    last push

---
+++ Leap Motion Test/Assets/Scripts/Shakes.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shakes : MonoBehaviour
+{
+
+    // pakt positie van de camera
+    public Transform camTransform;
+
+    // hoe lang de camera gaat shaken
+    private float shake = 0f;
+
+    // hoeveel en hoehard de camera gaat shaken
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    // de beginpositie van camera
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shake > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shake -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shake = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+
+    public void MakeShake(){
+        shake = 1;
+    }
+}
--- Leap Motion Test/Assets/Scripts/Spawning.cs
+++ Leap Motion Test/Assets/Scripts/Spawning.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 
 public class Spawning : MonoBehaviour
 {
-    [SerializeField]
     private float speed;
     private float tempFloat;
     private int Turn;
+
+    private DistanceCounter distanceCounter;
 
     private GameObject chunk1;
     private GameObject chunk2;
     private GameObject chunk3;
     private GameObject chunk4;
+
+    private Shakes shake;
 
     private List<GameObject> chunks;
 
@@ -20,6 +23,11 @@ public class Spawning : MonoBehaviour
 
     void Start()
     {
+
+        shake = GameObject.Find("Main Camera").GetComponent<Shakes>();
+
+        distanceCounter = new DistanceCounter();
+        
         chunk1 = Resources.Load<GameObject>("Chunk1");
         chunk2 = Resources.Load<GameObject>("Chunk2");
         chunk3 = Resources.Load<GameObject>("Chunk3");
@@ -33,16 +41,31 @@ public class Spawning : MonoBehaviour
         tempFloat = transform.position.z;
     }
 
+    //spawn new chunks when hitting the end
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "spawn")
+        {
+            SpawnNew();
+        }
+        
+    }
+
+    //deleting the old chunks
     void FixedUpdate()
     {
+        speed = distanceCounter.Speed;
+
         MoveDown();
-        if (transform.position.z > 40f)
+        if (transform.position.z > 50f)
         {
+            shake.MakeShake();
             SpawnNew();
             Destroy(gameObject);
         }
     }
 
+    //moving the chunks down
     private void MoveDown()
     {
         tempFloat += (Time.deltaTime * speed);
@@ -54,6 +77,9 @@ public class Spawning : MonoBehaviour
         Turn = Random.Range(0, 3);
         Debug.Log(chunks[Turn]);
         //Load();
-        Instantiate(chunks[Turn], new Vector3(0, 0f, -10f), chunks[Turn].transform.rotation);
+        Instantiate(chunks[Turn], new Vector3(5f, 0f, -10f), chunks[Turn].transform.rotation);
+
+        Turn = Random.Range(0, 3);
+        Instantiate(chunks[Turn], new Vector3(-5f, 0f, -10f), chunks[Turn].transform.rotation);
     }
 }
